d9b864f151efec85e14d0e79ce4ce8fbcdbf0d36
Phoenix Project <phoenixproject.erp@gmail.com>
2018-02-04 16:33:32 -0200

    Primeiro Commit

---
+++ MvcCoreOverview/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreOverview.Models;
+
+namespace MvcCoreOverview.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AnotherAction()
+        {
+            return View();
+        }
+
+		public IActionResult AnotherView()
+		{
+			return View("SomeView");
+		}
+
+		public IActionResult ViewWithParameter()
+		{
+			string SomeString = "I am a string";
+
+			return View("ViewWithParameter", SomeString);
+		}
+
+		public IActionResult PassingData()
+		{
+			ViewBag.Fruit = "Apples";
+			ViewData["Color"] = "Red";
+
+			TempData["Number"] = 5;
+
+			return View();
+		}
+
+		// localhost:59025/home/QueryString?name=Jonh&lastname=Jonhson
+		public IActionResult QueryString(string name, string lastname)
+		{
+			ViewBag.Name = name;
+
+			ViewBag.Lastname = lastname;
+			
+			return View();
+		}
+
+		public RedirectResult Redirect2()
+		{
+			return Redirect("http://www.google.com");
+		}
+
+		// Redirecionando para o outro controller
+		public IActionResult Redirect3()
+		{
+			return RedirectToAction("PassingData","AnotherController");
+		}
+
+		// Redirecionando para o outro controller passando um id
+		public IActionResult Redirect4()
+		{
+			return RedirectToAction("PassingData", "AnotherController", new { id = 1 });
+		}
+
+		public IActionResult Redirect()
+		{
+			return RedirectToAction("PassingData");
+		}
+	}
+}
---
+++ MvcCoreOverview/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcCoreOverview.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
